19beb36755bccea006b030b42e4eba505f3a2c56
Luiz Guilherme <luizgmelo64@gmail.com>
2025-02-07 11:44:11 -0300

    feat: create table products

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,18 @@ import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) {
+
+        var sql = "CREATE TABLE products (" +
+                "   id SERIAL PRIMARY KEY," +
+                "   name VARCHAR(255) NOT NULL," +
+                "   price DECIMAL(10, 2) NOT NULL" +
+                ");";
+
         try(var connection = Database.connect()) {
-            System.out.println("Connected to the PostgresSQL database.");
+            assert connection != null;
+            try(var stmt = connection.createStatement()) {
+                stmt.executeUpdate(sql);
+            }
         } catch (SQLException e) {
             System.err.println(e.getMessage());
         }
